14701cfe9cbdee1a61b1e4a970a5e9a674652f03
Regina Vivanco Lemus <revilem1331@gmail.com>
2025-03-21 21:27:22 -0600

    I made a component that renders faces based on a property

--- src/component/DiceComponent/DiceComponent.jsx
+++ src/component/DiceComponent/DiceComponent.jsx
@@ -1,69 +1,98 @@
 import React from "react"
 
 
-const DiceComponent = () => {
-    return (
-        <>
-            <div className="dice first-face">
-                <span className="dot">
-                </span>
-            </div>
-
-            <div className="dice second-face">
-                <span className="dot">
-                </span>
-                <span className="dot">
-                </span>
-            </div>
-
-            <div className="dice third-face">
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-            </div>
-
-            <div className="fourth-face dice">
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
+const DiceComponent = ({number}) => {
+    console.log(number)
+    if (number === 1) {
+        return (
+            <>
+                <div className="dice first-face">
+                    <span className="dot">
+                    </span>
                 </div>
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
+            </>
+        )
+    }
+    if (number === 2) {
+        return (
+            <>
+                <div className="dice second-face">
+                    <span className="dot">
+                    </span>
+                    <span className="dot">
+                    </span>
                 </div>
-            </div>
-            <div className="fifth-face dice">
-
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-
-                <div className="column">
-                    <span className="dot"></span>
-                </div>
-
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-
-            </div>
-            <div className="sixth-face dice">
-                <div className="column">
+            </>
+        )
+    }
+    if (number === 3) {
+        return (
+            <>
+                <div className="dice third-face">
                     <span className="dot"></span>
                     <span className="dot"></span>
                     <span className="dot"></span>
                 </div>
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                    <span className="dot"></span>
+            </>
+        )
+    }
+    if (number === 4) {
+        return (
+            <>
+                <div className="fourth-face dice">
+                    <div className="column">
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                    </div>
+                    <div className="column">
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                    </div>
                 </div>
+            </>
+        )
+    }
+    if (number === 5) {
+        return (
+            <>
 
-            </div>
-        </>
-    )
+                <div className="fifth-face dice">
+                    <div className="column">
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                    </div>
+
+                    <div className="column">
+                        <span className="dot"></span>
+                    </div>
+
+                    <div className="column">
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                    </div>
+                </div>
+            </>
+        )
+    }
+    if (number === 6) {
+        return (
+            <>
+                <div className="sixth-face dice">
+                    <div className="column">
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                    </div>
+                    <div className="column">
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                    </div>
+
+                </div>
+            </>
+        )
+    }
 }
 
 export default DiceComponent
